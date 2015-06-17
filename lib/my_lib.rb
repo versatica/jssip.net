0f51ff27c2c9eeffaf9cd54ec5f5ec0894d6730e
Iñaki Baz Castillo <ibc@aliax.net>
2015-06-17 13:04:43 +0200

    0.6.33

--- lib/my_lib.rb
+++ lib/my_lib.rb
@@ -2,7 +2,7 @@ module MyLib
 
   # Update these attributes for new stable releases.
   @jssip_last_version = "0.6.x"
-  @jssip_last_full_version = "0.6.32"
+  @jssip_last_full_version = "0.6.33"
 
   class << self
     attr_reader :jssip_last_version, :jssip_last_full_version
